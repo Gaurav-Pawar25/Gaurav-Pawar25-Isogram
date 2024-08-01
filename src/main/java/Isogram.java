ab933c6ed2061ff021a6fd9648e020f07f508fc2
Gaurav-Pawar25 <gauravpawar33915@gmail.com>
2024-08-01 14:11:57 +0530

    Done

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 
 public class Isogram {
     /**
@@ -13,6 +14,13 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        HashSet<Character> uniqueChar = new HashSet<>();
+        for(char c : str.toCharArray()){
+            if(uniqueChar.contains(c)){
+                return false;
+            }
+            uniqueChar.add(c);
+        }
+        return true;
     }
 }
